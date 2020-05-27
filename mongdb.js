16fe3d23e9dfdca44ac7a15d93ed3de806fdbfe8
Harrysurfing <harrwangok@gmail.com>
2020-05-27 21:55:20 +1000

    experiment with findone and find for mongodb

--- mongdb.js
+++ mongdb.js
@@ -4,35 +4,53 @@ const MongoClient = mongodb.MongoClient;
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-app";
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true }, (err, client) => {
-	if (err) {
-		return console.log("error connecting to mongodb");
-	}
-
-	console.log("connected successfully");
-
-	const db = client.db(databaseName);
-
-	db.collection("tasks").insertMany(
-		[
-			{
-				description: "task1",
-				completed: false,
-			},
-			{
-				description: "task2",
-				completed: false,
-			},
-			{
-				description: "task3",
-				completed: true,
-			},
-		],
-		(err, res) => {
-			if (err) {
-				return console.log(err);
-			}
-			console.log(res.ops);
+MongoClient.connect(
+	connectionURL,
+	{ useNewUrlParser: true, useUnifiedTopology: true },
+	(err, client) => {
+		if (err) {
+			return console.log("error connecting to mongodb");
 		}
-	);
-});
+
+		console.log("connected successfully");
+
+		const db = client.db(databaseName);
+
+		// db.collection("tasks").insertMany(
+		// 	[
+		// 		{
+		// 			description: "task1",
+		// 			completed: false,
+		// 		},
+		// 		{
+		// 			description: "task2",
+		// 			completed: false,
+		// 		},
+		// 		{
+		// 			description: "task3",
+		// 			completed: true,
+		// 		},
+		// 	],
+		// 	(err, res) => {
+		// 		if (err) {
+		// 			return console.log(err);
+		// 		}
+		// 		console.log(res.ops);
+		// 	}
+		// );
+
+		// db.collection("tasks").findOne({ description: "task1" }, (err, result) => {
+		// 	if (err) {
+		// 		return console.log(err);
+		// 	}
+
+		// 	console.log(result);
+		// });
+
+		db.collection("tasks")
+			.find({ completed: false })
+			.toArray((err, res) => {
+				console.log(res);
+			});
+	}
+);

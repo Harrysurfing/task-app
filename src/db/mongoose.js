3ccbfb4d43b741bb6f310ec6ac77d8f59c20797c
Harrysurfing <harrwangok@gmail.com>
2020-05-30 12:21:53 +1000

    set up a endpendpoint for post (create users)

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,49 +6,31 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-app-api", {
 	useCreateIndex: true,
 });
 
-// const User = mongoose.model("User", {
+// const Schema = mongoose.Schema;
+// const Animal = new Schema({
 // 	name: {
 // 		type: String,
+// 		default: "unkown",
 // 	},
 // 	age: {
 // 		type: Number,
+// 		min: 0,
+// 	},
+// 	illness: {
+// 		type: Boolean,
+// 		default: false,
 // 	},
 // });
 
-// const me = new User({ name: "harry", age: "12" });
+// const Cat = mongoose.model("Cat", Animal);
 
-// me.save()
+// const roti = new Cat({ name: "roti", age: 2 });
+
+// roti
+// 	.save()
 // 	.then((res) => {
 // 		console.log(res);
 // 	})
 // 	.catch((err) => {
 // 		console.log(err);
 // 	});
-const Schema = mongoose.Schema;
-const Animal = new Schema({
-	name: {
-		type: String,
-		default: "unkown",
-	},
-	age: {
-		type: Number,
-		min: 0,
-	},
-	illness: {
-		type: Boolean,
-		default: false,
-	},
-});
-
-const Cat = mongoose.model("Cat", Animal);
-
-const roti = new Cat({ name: "roti", age: 2 });
-
-roti
-	.save()
-	.then((res) => {
-		console.log(res);
-	})
-	.catch((err) => {
-		console.log(err);
-	});
